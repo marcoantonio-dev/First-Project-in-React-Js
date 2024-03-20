ca8941e702eeac29ba96c331abf37225b76f6026
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-03-20 09:19:23 -0300

    alteração mínima.

--- src/App.js
+++ src/App.js
@@ -15,11 +15,10 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          //href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
       <div className="App-footer">
