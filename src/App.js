ecfd02546c42553ec1e33b03eb3ba81336f3f2f0
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-03-20 14:06:44 -0300

    proejto concluido para a tarefa

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ function App() {
         <h1>Arquitetura 3D</h1>
       </div>
       <header className="App-body">
-        <img src={casa3d} className="Casa3D" alt="logo" />
-        <p>
-          Planeje projetos para a construção de casas ou prédios e muito mais
-        </p>
+          <img src={casa3d} className="Casa3D" alt="logo" />
+          <button className="Botao">Saiba Mais</button>
+        <div className="Texto">
+        <p className="Texto_na_imagem">
+        Planeje projetos para construções e muito mais. Com planejamento cuidadoso e visão clara, transforme ideias em realidade.
+        Cada etapa é crucial para o sucesso. Ao considerar todos os aspectos, garanta um resultado que atenda às suas necessidades. 
+        Com criatividade e dedicação, alcance resultados surpreendentes e crie espaços encantadores  
+         </p>
+        </div>
         <a
           className="App-link"
           //href="https://reactjs.org"
